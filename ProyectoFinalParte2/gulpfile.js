3a38b041aa12fe03cb91c3d5013a7c41e3dbf319
esnaider-blanco <304910730@cuc.cr>
2023-11-29 20:29:15 -0600

    primer commit con sass

---
+++ ProyectoFinalParte2/gulpfile.js
@@ -0,0 +1,14 @@
+﻿const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function compileSass() {
+    return gulp.src('sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('Css'));
+}
+
+gulp.task('default', compileSass);
+
+gulp.task('watch', function () {
+    gulp.watch('sass/*.scss', compileSass);
+});
